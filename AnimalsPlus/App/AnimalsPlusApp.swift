3a1f63659a343356655fe622bec40822bd2bfbfc
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-30 07:13:27 +0300

    Project Setup( images, colors, data, graphics, and setting for lunch and etc.)

---
+++ AnimalsPlus/App/AnimalsPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsPlusApp.swift
+//  AnimalsPlus
+//
+//  Created by Abdullah Bilgin on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
