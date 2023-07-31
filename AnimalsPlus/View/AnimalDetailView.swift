6ee2b26d1877f4fb792c6ad6d4c4262e6ce54ebf
Abdullah Bilgin <abilgin02@icloud.com>
2023-07-31 21:39:33 +0300

    Creatting animal detail view" with "Heading view"  and "insert gallery view" and imlement navigaion link on contentview to navigate detail views.

---
+++ AnimalsPlus/View/AnimalDetailView.swift
@@ -0,0 +1,74 @@
+//
+//  AnimalDetailView.swift
+//  AnimalsPlus
+//
+//  Created by Abdullah Bilgin on 7/31/23.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.primary)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.accentColor)
+                    .padding(.horizontal)
+                // GALLERY
+                Group {
+                    HeadingView(headingImage: "photo.on.rectangle.angled", headingText: "Wilderness in Picture")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                // LINK
+                
+            } //: VSTACK
+            .navigationBarTitle("Learn about \(animal.name)", displayMode: .inline)
+        } //: SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        NavigationView {
+            AnimalDetailView(animal: animals[0])
+        }
+    }
+}
