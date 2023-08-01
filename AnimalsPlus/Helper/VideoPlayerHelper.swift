d3b6cfc14740a7c463b812b63eff729bdb65e461
Abdullah Bilgin <abilgin02@icloud.com>
2023-08-01 20:53:15 +0300

    creating a video player view and videoplayer helper then implement to videoplayerview to navigate for each animal

---
+++ AnimalsPlus/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  AnimalsPlus
+//
+//  Created by Abdullah Bilgin on 8/1/23.
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
+
